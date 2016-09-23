36e12a0c9116ae9b5fa95f22c3f2ab58b2dfb081
Alexander Almaleh <sashoalm@gmail.com>
2016-09-23 15:59:26 +0300

    Test with QSortFilterProxyModel.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,36 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSortFilterProxyModel>
+#include <QStringListModel>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QStringList list;
+    for (int ii = 0; ii < 30000; ++ii) {
+        list.append(QString::number(qrand()));
+    }
+
+    QStringListModel *model = new QStringListModel(this);
+    model->setStringList(list);
+
+    QSortFilterProxyModel *filter = new QSortFilterProxyModel(this);
+    filter->setSourceModel(model);
+
+    ui->listView->setModel(filter);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_lineEdit_textChanged(const QString &text)
+{
+    QSortFilterProxyModel *filter = (QSortFilterProxyModel*) ui->listView->model();
+    filter->setFilterFixedString(text);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_lineEdit_textChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
 };
